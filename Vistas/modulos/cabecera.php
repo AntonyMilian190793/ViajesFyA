91bca4d61722341623a57b448fd9eea6fe6b362f
AntonyMilian190793 <antonymilian19@gmail.com>
2022-06-23 10:17:17 -0500

    commit

--- Vistas/modulos/cabecera.php
+++ Vistas/modulos/cabecera.php
@@ -2,7 +2,7 @@
     <!-- Logo -->
     <a href="Vistas/index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>CFY</b>A</span>
+      <span class="logo-mini"><b>VFY</b>A</span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>Viajes FYA</b></span>
     </a>
