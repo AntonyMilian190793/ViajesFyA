bf31539d7d730a7bdd193023980384e4a7453825
AntonyMilian190793 <antonymilian19@gmail.com>
2022-06-24 21:30:17 -0500

    commit

---
+++ viajesfya.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-06-2022 a las 04:29:01
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `viajesfya`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `otros`
+--
+
+CREATE TABLE `otros` (
+  `id` int(11) NOT NULL,
+  `usuario` text NOT NULL,
+  `clave` text NOT NULL,
+  `nombre` text NOT NULL,
+  `apellido` text NOT NULL,
+  `foto` text NOT NULL,
+  `rol` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `otros`
+--
+
+INSERT INTO `otros` (`id`, `usuario`, `clave`, `nombre`, `apellido`, `foto`, `rol`) VALUES
+(1, 'mary', '123', 'Maria', 'Portilla', '', 'Otros');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `otros`
+--
+ALTER TABLE `otros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `otros`
+--
+ALTER TABLE `otros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
