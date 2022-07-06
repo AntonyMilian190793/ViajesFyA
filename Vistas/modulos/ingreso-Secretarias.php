df48e3f44d40520cf350c6b3f1fa859bf4d0ddac
AntonyMilian190793 <51470801+AntonyMilian190793@users.noreply.github.com>
2022-07-06 17:55:42 -0500

    06-07-2022

---
+++ Vistas/modulos/ingreso-Secretarias.php
@@ -0,0 +1,39 @@
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><b>Viajes</b>FyA</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Ingresar como Secretarias</p>
+
+    <form method="post">
+
+      <div class="form-group has-feedback">
+
+        <input type="text" class="form-control" name="usuario-Ing" placeholder="Usuario">
+
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" name="clave-Ing" placeholder="Contraseña">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+
+        <!-- /.col -->
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
+        </div>
+        <!-- /.col -->
+      </div>
+
+    <?php
+
+        $ingreso = new OtrosC();
+        $ingreso -> IngresarOtrosC();
+    
+    ?>
+
+    </form>
+  </div>
+</div>
