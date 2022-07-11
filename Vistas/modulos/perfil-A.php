dbce561af42ea727b830ecb9e7eaec66a12310df
AntonyMilian190793 <antonymilian19@gmail.com>
2022-07-11 12:38:26 -0500

    11-07-2022

---
+++ Vistas/modulos/perfil-A.php
@@ -0,0 +1,37 @@
+<?php
+
+if ($_SESSION["rol"] != "Administracion") {
+
+    echo '<script>
+        
+        window.location = inicio;
+        </script>';
+
+    return;
+}
+
+?>
+
+<div class="content-wrapper">
+
+    <section class="content">
+
+        <div class="box">
+
+            <div class="box-body">
+
+            <?php
+
+            $editarPerfil = new AdministracionC();
+            $editarPerfil -> EditarPerfilAdministracionC();
+            // $editarPerfil -> ActualizarPerfilOtrosC();
+
+            ?>
+
+
+    
+            </div>
+        </div>
+    </section>
+
+</div>
