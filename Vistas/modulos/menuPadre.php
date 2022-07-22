2920ea0e9fc038887298a3bec7d3110ed968f5af
AntonyMilian190793 <antonymilian19@gmail.com>
2022-07-22 09:19:17 -0500

    22-07-2022

---
+++ Vistas/modulos/menuPadre.php
@@ -0,0 +1,33 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+    <ul class="sidebar-menu">
+      <li>
+        <a href="http://localhost/ViajesFyA/inicio">
+          <i class="fa fa-home"></i>
+          <span>Inicio</span>
+        </a>
+      </li>
+
+
+      <li>
+        <a href="http://localhost/ViajesFyA/Citas">
+          <i class="fa fa-address-book-o"></i>
+          <span>Vuelos</span>
+        </a>
+      </li>
+
+      <li>
+        
+          <a href="http://localhost/ViajesFyA/otrosTrabajadores">
+          <i class="fa fa-calendar-check-o"></i>
+          <span>Trabajadores</span>
+        </a>
+      </li>
+
+    </ul>
+      
+    </section>
+    <!-- /.sidebar -->
+  </aside>
