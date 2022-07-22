e4cde551cc208e31a3293721cf2d0975ab4c6206
AntonyMilian190793 <antonymilian19@gmail.com>
2022-07-22 14:52:28 -0500

    22-07-2022

---
+++ Vistas/modulos/perfil-P.php
@@ -0,0 +1,39 @@
+<?php
+
+if ($_SESSION["rol"] != "Padre") {
+
+    echo '<script>
+        
+        window.location = inicio;
+        </script>';
+
+    return;
+}
+
+?>
+
+<div class="content-wrapper">
+
+    <section class="content">
+
+        <div class="box">
+
+            <div class="box-body">
+
+            <?php
+
+            $editarPerfil = new PadresC();
+            $editarPerfil -> EditarPerfilPadreC();
+            // $editarPerfil -> ActualizarPerfilOtrosC();
+            
+
+            ?>
+
+
+
+
+            </div>
+        </div>
+    </section>
+
+</div>
