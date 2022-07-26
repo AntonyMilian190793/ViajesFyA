6783d13e248b1e2472f92376e8b03d51dcfad132
AntonyMilian190793 <antonymilian19@gmail.com>
2022-07-26 11:13:25 -0500

    26-07-2022

---
+++ Vistas/modulos/perfil-S.php
@@ -0,0 +1,39 @@
+<?php
+
+if ($_SESSION["rol"] != "Sistemas") {
+
+    echo '<script>
+        
+        window.location = inicio;
+        </script>';
+
+    return;
+}
+
+?>
+
+<div class="content-wrapper">
+
+    <section class="content">
+
+        <div class="box">
+
+            <div class="box-body">
+
+            <?php
+
+            $editarPerfil = new SistemasC();
+            $editarPerfil -> EditarPerfilSistemasC();
+            //$editarPerfil -> ActualizarPerfilPadreC();
+            
+
+            ?>
+
+
+
+
+            </div>
+        </div>
+    </section>
+
+</div>
