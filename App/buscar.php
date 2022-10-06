bed587b54d038f80eb0f69b00271403d9366dfa6
AntonyMilian190793 <51470801+AntonyMilian190793@users.noreply.github.com>
2022-10-05 21:42:58 -0500

    05-10-2022

---
+++ App/buscar.php
@@ -0,0 +1,56 @@
+
+<?php
+
+$mysqli = new mysqli("localhost", "root", "", "viajesfya");
+$salida = "";
+$query = "SELECT * FROM vueloslogistica ORDER BY id";
+
+if(isset($_POST['consulta'])){
+    $q = $mysqli->real_escape_string($_POST['consulta']);
+    $query = "SELECT id, nyaP, documento, colegio, inicio, fin FROM vueloslogistica WHERE nyaP LIKE '%" . $q . "%' OR nyaP LIKE '%" . $q . "%' 
+    OR documento LIKE '%" . $q . "%'";
+}
+
+    $resultado = $mysqli->query($query);
+
+    if($resultado->num_rows > 0){
+
+        $salida.="<table class='tabla_datos table -table-bordered table-hover table-striped DT'>
+        <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Documento</th>
+                    <th>Colegio</th>
+                    <th>Inicio</th>
+                    <th>Fin</th>
+                </tr>
+                </thead>
+                <tbody>";
+
+    
+    while($fila = $resultado->fetch_assoc()){
+        $salida.= "<tr>
+        <td>". $fila['id']. "</td>
+        <td>". $fila['nyaP']. "</td>
+        <td>". $fila['documento']. "</td>
+        <td>". $fila['colegio']."</td>
+        <td>". $fila['inicio']."</td>
+        <td>". $fila['fin']."</td>
+        </tr>";
+        }
+
+    $salida.="</tbody></table>";
+
+    }else{
+
+        $salida.="No existen datos...";
+    }
+
+    echo $salida;
+    $mysqli->close();
+
+
+
+
+?>
