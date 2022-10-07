bd0bc06eeb010c48297228dfdbf03bec41ad399f
AntonyMilian190793 <antonymilian19@gmail.com>
2022-10-07 15:35:37 -0500

    07-10-2022

---
+++ Vistas/js/editarCitas.js
@@ -0,0 +1,18 @@
+$(".DTE").DataTable({
+    "language": {
+        sSearch: "Buscar:",
+        sEmptyTable: "No hay datos disponibles",
+        sZeroRecords: "No se encontraron resultados",
+        sInfo: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando 0 a 0 de 0 registros",
+        SInfoFiltered: "(Filtrado de _MAX_ registros)",
+        oPaginate: {
+            sFirst: "Primero",
+            sLast: "Último",
+            sNext: "Siguiente",
+            sPrevious: "Anterior"
+        },
+        sLoadingRecords: "Cargando...",
+        sLengthMenu: "Mostrar _MENU_ Registros",
+    }
+});
---
+++ Vistas/modulos/VuelosTabla.php
@@ -0,0 +1,82 @@
+<?php
+
+if ($_SESSION["rol"] != "Logistica") {
+
+    echo '<script>
+  
+  window.location = "inicio";
+
+  </script>';
+}
+
+?>
+
+
+
+<div class="content-wrapper">
+
+    <section class="content-header">
+        <h1>Gestor de otros Trabajadores</h1>
+    </section>
+
+    <section class="content">
+        <div class="box">
+            <div class="box-header">
+
+
+                
+            </div>
+            <div class="box-body">
+                <table class="table -table-bordered table-hover table-striped DTE">
+                    <thead>
+                        <tr>
+                            <th>N°</th>
+                            <th>Nombre</th>
+                            <th>Documento</th>
+                            <th>Colegio</th>
+                            <th>Fecha Inicio</th>
+                            <th>Fecha Fin</th>
+                            <th>Editar / Borrar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php
+
+                        $columna = null;
+                        $valor = null;
+
+                        $resultado = CitasC::VerVuelosLogisticaC($columna, $valor);
+
+                        foreach ($resultado as $key => $value) {
+
+                            echo '<tr>
+                            <td>'.($key+1).'</td>
+                            <td>'.$value["nyaP"].'</td>
+                            <td>'.$value["documento"].'</td>
+                            <td>'.$value["colegio"].'</td>
+                            <td>'.$value["inicio"].'</td>
+                            <td>'.$value["fin"].'</td>
+                            <td>
+                            <div class="btn-group">
+                                <button class="btn btn-success"><i class="fa fa-pencil"> 
+                                Editar</i></button>
+
+                                
+                                
+
+                                        <button class="btn btn-danger">
+                                        <i class="fa fa-times"> Borrar</i></button>
+                                    </div>
+                            </td>
+                        </tr>';
+                        }
+
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+
+</div>
