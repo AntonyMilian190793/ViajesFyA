043f45c785de073f4668b8a181e7c303d24603fe
AntonyMilian190793 <antonymilian19@gmail.com>
2022-10-10 08:27:42 -0500

    10-02-2022

---
+++ Vistas/js/citas.js
@@ -0,0 +1,25 @@
+$(".DTE").on("click", ".EliminarVuelo", function() {
+    var Pid = $(this).attr("Pid");
+
+    window.location = "index.php?url=VuelosTabla&Pid=" + Pid;
+});
+
+
+$(".DTE").DataTable({
+    "language": {
+        sSearch: "Buscar:",
+        sEmptyTable: "No hay datos disponibles",
+        sZeroRecords: "No se encontraron resultados",
+        sInfo: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando 0 a 0 de 0 registros",
+        SInfoFiltered: "(Filtrado de _MAX_ registros)",
+        oPaginate: {
+            sFirst: "Primero",
+            sLast: "Último",
+            sNext: "Siguiente",
+            sPrevious: "Anterior"
+        },
+        sLoadingRecords: "Cargando...",
+        sLengthMenu: "Mostrar _MENU_ Registros",
+    }
+});
