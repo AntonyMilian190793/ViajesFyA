92d7dbec13ba2aa941c38e3cfc569d1d1631709c
AntonyMilian190793 <antonymilian19@gmail.com>
2022-11-22 11:35:28 -0500

    22-11-2022

--- Vistas/modulos/cabecera.php
+++ Vistas/modulos/cabecera.php
@@ -2,9 +2,9 @@
     <!-- Logo -->
     <a href="http://localhost/ViajesFyA/inicio" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>VFY</b>A</span>
+      <span class="logo-mini"><b>T</b>K</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Viajes FYA</b></span>
+      <span class="logo-lg"><b>Thakhi Kusikuywan</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
