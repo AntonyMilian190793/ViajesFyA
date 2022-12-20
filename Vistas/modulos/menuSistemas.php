7328f843a22fbbc6a9cbac110fdde134d1d3b1e7
AntonyMilian190793 <antonymilian19@gmail.com>
2022-12-20 10:23:54 -0500

    20-12-2022

--- Vistas/modulos/menuSistemas.php
+++ Vistas/modulos/menuSistemas.php
@@ -13,42 +13,35 @@
         <li>
         <a href="http://localhost/ViajesFyA/consultas">
           <i class="fa fa-address-book-o"></i>
-          <span>Áreas</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="http://localhost/ViajesFyA/CalendarioG">
-        <i class="fa fa-calendar" aria-hidden="true"></i>
-          <span>Calendario Global</span>
+          <span>Gestionar Áreas</span>
         </a>
       </li>
 
       <li>
         <a href="http://localhost/ViajesFyA/colegios">
           <i class="fa fa-university" aria-hidden="true"></i>
-          <span>Colegios</span>
+          <span>Gestionar Colegios</span>
         </a>
       </li>
 
       <li>
         <a href="http://localhost/ViajesFyA/padres">
           <i class="fa fa-user"></i>
-          <span>Directivos</span>
+          <span>Gestionar Directivos</span>
         </a>
       </li>
 
       <li>
         <a href="http://localhost/ViajesFyA/administracion">
           <i class="fa fa-male" aria-hidden="true"></i>
-          <span>Jefes de Área</span>
+          <span>Gestionar Jefes de Área</span>
         </a>
       </li>
 
       <li>
         <a href="http://localhost/ViajesFyA/secretarias">
           <i class="fa fa-user-times"></i>
-          <span>Logística</span>
+          <span>Gestionar Logística</span>
         </a>
       </li>
 
@@ -56,7 +49,7 @@
       <li>
         <a href="http://localhost/ViajesFyA/otrosTrabajadores">
           <i class="fa fa-users"></i>
-          <span>Colaboradores</span>
+          <span>Gestionar Colaboradores</span>
         </a>
       </li>
 
@@ -64,6 +57,13 @@
         <a href="http://localhost/ViajesFyA/Ver-areas">
           <i class="fa fa-address-book-o"></i>
           <span>Ver todas las Áreas</span>
+        </a>
+      </li>
+
+            <li>
+        <a href="http://localhost/ViajesFyA/CalendarioG">
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+          <span>Calendario Global</span>
         </a>
       </li>
 
