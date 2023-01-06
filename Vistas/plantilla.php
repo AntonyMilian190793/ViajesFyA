1c5e1328fa214ab0346b55298d98c89a94c87e58
AntonyMilian190793 <antonymilian19@gmail.com>
2023-01-06 09:10:25 -0500

    06-01-2023

--- Vistas/plantilla.php
+++ Vistas/plantilla.php
@@ -8,6 +8,7 @@ session_start();
 <html lang="es">
 
 <head>
+  <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>CHAYANAN</title>
