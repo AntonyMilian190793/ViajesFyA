4aa25d70ddd9e1c170224af31a02d22d88c5ab53
AntonyMilian190793 <antonymilian19@gmail.com>
2023-01-17 17:02:25 -0500

    17-01-2023

--- Modelos/inicioM.php
+++ Modelos/inicioM.php
@@ -7,7 +7,7 @@ require_once "ConexionBD.php";
 
         static public function MostrarInicioM($tablaBD, $id){
 
-            $pdo = ConexionBD::cBD()->prepare("SELECT id, intro, horaE, horaS, direccion, telefono, correo, logo, favicon FROM $tablaBD 
+            $pdo = ConexionBD::cBD()->prepare("SELECT id, intro, horaE, horaS, direccion, telefono, telefono1, correo, correo1, logo, favicon FROM $tablaBD 
             WHERE id = :id");
 
             $pdo->bindParam(":id", $id, PDO::PARAM_INT);
@@ -21,8 +21,8 @@ require_once "ConexionBD.php";
         //ctulizar inicio
 	static public function ActualizarInicioM($tablaBD, $datosC){
 
-		$pdo = ConexionBD::cBD()->prepare("UPDATE $tablaBD SET intro = :intro, direccion = :direccion, horaE = :horaE, horaS = :horaS, telefono = :telefono, 
-        correo = :correo, logo = :logo, favicon = :favicon WHERE id = :id");
+		$pdo = ConexionBD::cBD()->prepare("UPDATE $tablaBD SET intro = :intro, direccion = :direccion, horaE = :horaE, horaS = :horaS, 
+		telefono = :telefono, correo = :correo, telefono1 = :telefono1, correo1 = :correo1, logo = :logo, favicon = :favicon WHERE id = :id");
 
 		$pdo -> bindParam(":id", $datosC["id"], PDO::PARAM_INT);
 		$pdo -> bindParam(":intro", $datosC["intro"], PDO::PARAM_STR);
@@ -31,6 +31,8 @@ require_once "ConexionBD.php";
 		$pdo -> bindParam(":horaS", $datosC["horaS"], PDO::PARAM_STR);
 		$pdo -> bindParam(":telefono", $datosC["telefono"], PDO::PARAM_STR);
 		$pdo -> bindParam(":correo", $datosC["correo"], PDO::PARAM_STR);
+		$pdo -> bindParam(":telefono1", $datosC["telefono1"], PDO::PARAM_STR);
+		$pdo -> bindParam(":correo1", $datosC["correo1"], PDO::PARAM_STR);
 		$pdo -> bindParam(":logo", $datosC["logo"], PDO::PARAM_STR);
 		$pdo -> bindParam(":favicon", $datosC["favicon"], PDO::PARAM_STR);
 
