656143503ca97d7dd5e6d24e2b21c9e761defa26
AntonyMilian190793 <51470801+AntonyMilian190793@users.noreply.github.com>
2023-02-02 09:40:22 -0500

    02-02-2023

--- Modelos/ConexionBD.php
+++ Modelos/ConexionBD.php
@@ -5,8 +5,7 @@ class ConexionBD{
     public function cBD(){
 
         $bd = new PDO("mysql:host=localhost;dbname=viajesfya", "root", "");
-
-        $bd->exec("SET lc_time_names = 'es_PE'");
+        $bd->exec("SET CHARACTER SET utf8mb4");
         return $bd;
     }
 }
