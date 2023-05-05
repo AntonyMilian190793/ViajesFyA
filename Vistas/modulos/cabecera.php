818bf3ed53b5c649a01060e61ae5a6b3126cdece
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-05-05 15:55:45 -0500

    05-05-2023

--- Vistas/modulos/cabecera.php
+++ Vistas/modulos/cabecera.php
@@ -4,7 +4,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>CHA</b>FYA</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>CHAYANAN</b></span>
+      <span class="logo-lg"><img src="img/logo1.png" height="50px" weight="200px"></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
